ba3675b01034d911d68bb1615f0f917d6e6dbd3f
AshkanKiafard <ashkan.game.dev@gmail.com>
2026-05-15 15:54:56 +0200

    changed base models in evaluation.py

--- code/evaluation/evaluation.py
+++ code/evaluation/evaluation.py
@@ -32,9 +32,13 @@ LIGHTNING_DIR = "data/models/lightning"
 
 base_models = [
     "sentence-transformers/all-mpnet-base-v2",
+    # "BAAI/bge-base-en-v1.5",
+    # "ibm-granite/granite-embedding-small-english-r2",
     "BAAI/bge-large-en-v1.5",
+    "ibm-granite/granite-embedding-english-r2",
     "mixedbread-ai/mxbai-embed-large-v1",
     "Qwen/Qwen3-Embedding-0.6B",
+    # "Qwen/Qwen3-Embedding-4B",
 ]
 
 
