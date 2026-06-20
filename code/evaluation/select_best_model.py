e654881121f9570c0dbaed8dad615705a23bf329
AshkanKiafard <ashkan.game.dev@gmail.com>
2026-06-20 16:48:57 +0200

    Refactor: removed webis related binary_causal_extractor.py

--- code/evaluation/select_best_model.py
+++ code/evaluation/select_best_model.py
@@ -352,7 +352,7 @@ def parse_args():
     parser.add_argument(
         "evaluation_results_path",
         nargs="?",
-        default="data/evaluation/causenet/msmarco_valid/best_v2/evaluation_results.json",
+        default="data/evaluation/causenet/msmarco_valid/v3/evaluation_results.json",
         help="Path to validation evaluation_results.json.",
     )
     parser.add_argument(
